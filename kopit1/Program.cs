10a249c4fe108bede082a2c6159084472b869a18
semen.jons <semen.jons@yandex.ru>
2022-11-22 21:50:26 +0300

    добави1

---
+++ kopit1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите  число А:");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число В:");
+int B = int.Parse(Console.ReadLine());
+
+if (A > B)
+{
+    Console.WriteLine("Да, A является max");
+}
+else
+{
+    Console.WriteLine("Да, B является max");
+}
