c2ad1cf2348405e71555fd607951f170dabe04e5
semen.jons <semen.jons@yandex.ru>
2022-11-22 22:01:14 +0300

    добави4

---
+++ kopit4/Program.cs
@@ -0,0 +1,19 @@
+﻿ {
+    Console.Write("Введите  число:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int res = 1;
+       
+        if (res == 1)
+
+        {
+         res += 1;
+        }
+
+        while (res <= num)
+
+        {
+            Console.WriteLine(res);
+            res += 2;
+        }
+
+    }
